1bff2008b91e2116da73ee7f1119542c0bb612f9
Jeff L <jeff.jeff.mail@gmail.com>
2010-07-11 02:09:45 -0400

    Device panels are totally dynamic now

---
+++ src/ez_lights/gui/ControllableDevicePanel.java
@@ -0,0 +1,80 @@
+package ez_lights.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle;
+
+public class ControllableDevicePanel extends javax.swing.JPanel {
+
+	private GroupLayout groupLayout;
+	private JButton OffButton;
+	private JButton OnButton;
+	private JTextField DeviceName;
+	
+	public ControllableDevicePanel() {
+		super();
+		initPanel("Unknown");
+	}
+	
+	public ControllableDevicePanel(String deviceNameText) {
+		super();
+		initPanel(deviceNameText);
+	}
+	
+	public GroupLayout getGroupLayout() {
+		return groupLayout;
+	}
+	
+	private void initPanel(String deviceNameText) {
+
+		DeviceName = new JTextField();
+		DeviceName.setText(deviceNameText);
+
+		OnButton = new JButton();
+		OnButton.setText("On");
+		OnButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println(DeviceName.getText() + " On button pushed.");
+			}
+		});
+
+		OffButton = new JButton();
+		OffButton.setText("Off");
+		OffButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println(DeviceName.getText() + " Off button pushed.");
+			}
+		});
+		
+		groupLayout = new GroupLayout((JComponent)this);
+		this.setLayout(groupLayout);
+		this.setBorder( BorderFactory.createEtchedBorder() );
+
+		groupLayout.setVerticalGroup(groupLayout.createSequentialGroup()
+				.addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(DeviceName, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(OnButton, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(OffButton, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+				)
+		);	
+		
+		groupLayout.setHorizontalGroup(groupLayout.createSequentialGroup()
+				.addGroup(groupLayout.createSequentialGroup()
+						.addComponent(DeviceName, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE))				.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+				.addGroup(groupLayout.createSequentialGroup()
+						.addComponent(OnButton, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+				.addGroup(groupLayout.createSequentialGroup()
+						.addComponent(OffButton, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE))
+		);		
+	
+	}
+	
+}
--- src/ez_lights/gui/EZ_LightsMainFrame.java
+++ src/ez_lights/gui/EZ_LightsMainFrame.java
@@ -1,6 +1,5 @@
 package ez_lights.gui;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.AbstractAction;
 import javax.swing.GroupLayout;
@@ -10,33 +9,18 @@ import javax.swing.JDialog;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.LayoutStyle;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
 
-/**
-* This code was edited or generated using CloudGarden's Jigloo
-* SWT/Swing GUI Builder, which is free for non-commercial
-* use. If Jigloo is being used commercially (ie, by a corporation,
-* company or business for any purpose whatever) then you
-* should purchase a license for each developer using Jigloo.
-* Please visit www.cloudgarden.com for details.
-* Use of Jigloo implies acceptance of these licensing terms.
-* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
-* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
-* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
-*/
 public class EZ_LightsMainFrame extends javax.swing.JFrame {
 	private JButton CloseButton;
-	private JButton OnButton;
-	private JTextField DeviceName;
-	private AbstractAction closeMainFrame;
 	private AbstractAction closeAboutAction;
-	private JPanel jPanel1;
+	private ControllableDevicePanel controllableDevicePanel1;
+	private ControllableDevicePanel controllableDevicePanel2;
+	private ControllableDevicePanel controllableDevicePanel3;
 	private JTextField jTextField1;
 	private JButton OKButton;
 	private JDialog AboutDialog;
@@ -45,129 +29,95 @@ public class EZ_LightsMainFrame extends javax.swing.JFrame {
 	private JMenu HelpMenu;
 	private JMenu jMenu1;
 	private JMenuBar jMenuBar1;
-	private JButton OffButton;
 
-	/**
-	* Auto-generated main method to display this JFrame
-	*/
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				EZ_LightsMainFrame inst = new EZ_LightsMainFrame();
-				inst.setLocationRelativeTo(null);
-				inst.setVisible(true);
+				EZ_LightsMainFrame myFrame = new EZ_LightsMainFrame();
+				myFrame.setLocationRelativeTo(null);
+				myFrame.setVisible(true);
 			}
 		});
 	}
-	
+
 	public EZ_LightsMainFrame() {
 		super();
 		initGUI();
 	}
-	
+
 	private void initGUI() {
 		try {
 			GroupLayout thisLayout = new GroupLayout((JComponent)getContentPane());
 			getContentPane().setLayout(thisLayout);
 			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+			jMenuBar1 = new JMenuBar();
+			setJMenuBar(jMenuBar1);
 			{
-				jMenuBar1 = new JMenuBar();
-				setJMenuBar(jMenuBar1);
+				jMenu1 = new JMenu();
+				jMenuBar1.add(jMenu1);
+				jMenu1.setText("File");
+
+				HelpMenu = new JMenu();
+				jMenuBar1.add(HelpMenu);
+				HelpMenu.setText("Help");
 				{
-					jMenu1 = new JMenu();
-					jMenuBar1.add(jMenu1);
-					jMenu1.setText("File");
-				}
-				{
-					HelpMenu = new JMenu();
-					jMenuBar1.add(HelpMenu);
-					HelpMenu.setText("Help");
-					{
-						jMenuItem1 = new JMenuItem();
-						HelpMenu.add(jMenuItem1);
-						jMenuItem1.setText("jMenuItem1");
-						jMenuItem1.setAction(getAboutAction());
-					}
+					jMenuItem1 = new JMenuItem();
+					HelpMenu.add(jMenuItem1);
+					jMenuItem1.setText("jMenuItem1");
+					jMenuItem1.setAction(getAboutAction());
 				}
 			}
-			{
-				CloseButton = new JButton();
-				CloseButton.setText("Close");
-		//		CloseButton.setAction(getCloseMainFrame());
-				CloseButton.setAction(new AbstractAction("Close", null) {
-					public void actionPerformed(ActionEvent evt) {
-						setVisible(false);
-						dispose();
-						System.exit(0);
-					}
-				});
-			}
-			{
-				jPanel1 = new JPanel();
-				GroupLayout jPanel1Layout = new GroupLayout((JComponent)jPanel1);
-				jPanel1.setLayout(jPanel1Layout);
-				{
-					DeviceName = new JTextField();
-					DeviceName.setText("Device 1");
+			CloseButton = new JButton();
+			CloseButton.setText("Close");
+			//		CloseButton.setAction(getCloseMainFrame());
+			CloseButton.setAction(new AbstractAction("Close", null) {
+				public void actionPerformed(ActionEvent evt) {
+					setVisible(false);
+					dispose();
+					System.exit(0);
 				}
-				{
-					OnButton = new JButton();
-					OnButton.setText("On");
-					OnButton.addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent e) {
-							System.out.println(DeviceName.getText() + " On button pushed.");
-						}
-					});					
-				}
-				{
-					OffButton = new JButton();
-					OffButton.setText("Off");
-					OffButton.addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent e) {
-							System.out.println(DeviceName.getText() + " Off button pushed.");
-						}
-					});
-				}
-				jPanel1Layout.setVerticalGroup(jPanel1Layout.createSequentialGroup()
-						.addGroup(jPanel1Layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-								.addComponent(DeviceName, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(OnButton, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(OffButton, GroupLayout.Alignment.BASELINE, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)));
-				jPanel1Layout.setHorizontalGroup(jPanel1Layout.createSequentialGroup()
-						.addGroup(jPanel1Layout.createSequentialGroup()
-								.addComponent(DeviceName, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE))
-								.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-								.addGroup(jPanel1Layout.createSequentialGroup()
-										.addComponent(OnButton, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE))
-										.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-										.addGroup(jPanel1Layout.createSequentialGroup()
-												.addComponent(OffButton, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)));
-			}
+			});
+
+			controllableDevicePanel1 = new ControllableDevicePanel("Device Name 1");
+			controllableDevicePanel2 = new ControllableDevicePanel("Device Name 2");
+			controllableDevicePanel3 = new ControllableDevicePanel();
+					
+
 			thisLayout.setVerticalGroup(thisLayout.createSequentialGroup()
-				.addContainerGap()
-				.addComponent(jPanel1, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
-				.addGap(201)
-				.addComponent(CloseButton, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
-				.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
+					.addContainerGap()
+					.addComponent(controllableDevicePanel1, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+					.addComponent(controllableDevicePanel2, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+					.addComponent(controllableDevicePanel3, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+					.addGap(10)
+					.addComponent(CloseButton, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
 			thisLayout.setHorizontalGroup(thisLayout.createSequentialGroup()
-				.addContainerGap()
-				.addGroup(thisLayout.createParallelGroup()
-				    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
-				        .addComponent(jPanel1, GroupLayout.PREFERRED_SIZE, 301, GroupLayout.PREFERRED_SIZE)
-				        .addGap(0, 66, Short.MAX_VALUE))
-				    .addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
-				        .addGap(293)
-				        .addComponent(CloseButton, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
-				        .addGap(0, 0, Short.MAX_VALUE)))
-				.addGap(7));
+					.addContainerGap()
+					.addGroup(thisLayout.createParallelGroup()
+							.addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+									.addComponent(controllableDevicePanel1, GroupLayout.PREFERRED_SIZE, 301, GroupLayout.PREFERRED_SIZE)
+									.addGap(0, 66, Short.MAX_VALUE))
+							.addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+									.addComponent(controllableDevicePanel2, GroupLayout.PREFERRED_SIZE, 301, GroupLayout.PREFERRED_SIZE)
+									.addGap(0, 66, Short.MAX_VALUE))
+							.addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+									.addComponent(controllableDevicePanel3, GroupLayout.PREFERRED_SIZE, 301, GroupLayout.PREFERRED_SIZE)
+									.addGap(0, 66, Short.MAX_VALUE))									
+							.addGroup(GroupLayout.Alignment.LEADING, thisLayout.createSequentialGroup()
+									.addGap(293)
+									.addComponent(CloseButton, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
+									.addGap(0, 0, Short.MAX_VALUE))
+							)
+					.addGap(7));
 			pack();
-			this.setSize(396, 322);
+		//	this.setSize(396, 322);
 		} catch (Exception e) {
-		    //add your error handling code here
+			//add your error handling code here
 			e.printStackTrace();
 		}
 	}
-	
+
 	private AbstractAction getAboutAction() {
 		if(aboutAction == null) {
 			aboutAction = new AbstractAction("About", null) {
@@ -180,7 +130,7 @@ public class EZ_LightsMainFrame extends javax.swing.JFrame {
 		}
 		return aboutAction;
 	}
-	
+
 	private JDialog getAboutDialog() {
 		if(AboutDialog == null) {
 			AboutDialog = new JDialog(this);
@@ -198,23 +148,23 @@ public class EZ_LightsMainFrame extends javax.swing.JFrame {
 				jTextField1.setHorizontalAlignment(SwingConstants.CENTER);
 			}
 			AboutDialogLayout.setHorizontalGroup(AboutDialogLayout.createSequentialGroup()
-				.addContainerGap()
-				.addGroup(AboutDialogLayout.createParallelGroup()
-				    .addComponent(jTextField1, GroupLayout.Alignment.LEADING, 0, 306, Short.MAX_VALUE)
-				    .addGroup(GroupLayout.Alignment.LEADING, AboutDialogLayout.createSequentialGroup()
-				        .addGap(0, 239, Short.MAX_VALUE)
-				        .addComponent(OKButton, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)))
-				.addContainerGap());
+					.addContainerGap()
+					.addGroup(AboutDialogLayout.createParallelGroup()
+							.addComponent(jTextField1, GroupLayout.Alignment.LEADING, 0, 306, Short.MAX_VALUE)
+							.addGroup(GroupLayout.Alignment.LEADING, AboutDialogLayout.createSequentialGroup()
+									.addGap(0, 239, Short.MAX_VALUE)
+									.addComponent(OKButton, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)))
+									.addContainerGap());
 			AboutDialogLayout.setVerticalGroup(AboutDialogLayout.createSequentialGroup()
-				.addContainerGap(39, 39)
-				.addComponent(jTextField1, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
-				.addGap(0, 72, Short.MAX_VALUE)
-				.addComponent(OKButton, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
-				.addContainerGap());
+					.addContainerGap(39, 39)
+					.addComponent(jTextField1, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
+					.addGap(0, 72, Short.MAX_VALUE)
+					.addComponent(OKButton, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap());
 		}
 		return AboutDialog;
 	}
-	
+
 	private AbstractAction getCloseAboutAction() {
 		if(closeAboutAction == null) {
 			closeAboutAction = new AbstractAction("OK", null) {
@@ -225,8 +175,8 @@ public class EZ_LightsMainFrame extends javax.swing.JFrame {
 		}
 		return closeAboutAction;
 	}
-	
-/*	private AbstractAction getCloseMainFrame() {
+
+	/*	private AbstractAction getCloseMainFrame() {
 		if(closeMainFrame == null) {
 			closeMainFrame = new AbstractAction("Close", null) {
 				public void actionPerformed(ActionEvent evt) {
@@ -238,6 +188,6 @@ public class EZ_LightsMainFrame extends javax.swing.JFrame {
 		}
 		return closeMainFrame;
 	}
-	*/
+	 */
 
 }
